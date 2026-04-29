ffeff8eb07e43d384dec95b0838dd4ccbe62350b
alexrivas05 <alex12rivas34@gmail.com>
2026-04-28 23:26:31 -0400

    Added Section wrapper component with forwarded props. Applied it to Examples and CoreConcepts

--- src/components/CoreConcepts/CoreConcepts.jsx
+++ src/components/CoreConcepts/CoreConcepts.jsx
@@ -1,15 +1,15 @@
 import { CORE_CONCEPTS } from "../../data";
 import CoreConcept from './CoreConcept';
+import Section from '../Section/Section';
+
 export default function CoreConcepts() {
-    return (
-        <section id="core-concepts">
-            <h2>Time to get started!</h2>
-            <ul>
-                {
-                    CORE_CONCEPTS.map((conceptItem) =>
-                        <CoreConcept key={conceptItem.title} {...conceptItem} />)
-                }
-            </ul>
-        </section>
-    )
+  return (
+    <Section id="core-concepts" title="Time to get started!">
+      <ul>
+        {CORE_CONCEPTS.map((conceptItem) =>
+          <CoreConcept key={conceptItem.title} {...conceptItem} />
+        )}
+      </ul>
+    </Section>
+  );
 }
