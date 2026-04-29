f82fc62093e3372e1799b8f9ee8db1a955dceb4d
alexrivas05 <alex12rivas34@gmail.com>
2026-04-28 23:16:06 -0400

    Extracted examples into it's own componet

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,16 @@
-import { CORE_CONCEPTS, EXAMPLES } from './data';
 import Header from './components/Header/Header';
-import CoreConcept from './components/CoreConcepts/CoreConcept';
-import TabButton from './components/TabButton/TabButton';
-import { useState } from 'react';
 import CoreConcepts from './components/CoreConcepts/CoreConcepts';
-function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-  let tabContent = <p>Please Select a topic</p>;
-  if(selectedTopic){
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>
-            {EXAMPLES[selectedTopic].code}
-          </code>
-        </pre>
+import Examples from './components/Examples/Examples';
 
-      </div>
-    )
-  }
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-    console.log(selectedTopic)
-  }
+function App() {
   return (
-    <div>
+    <>
       <Header />
       <main>
         <CoreConcepts />
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={selectedTopic==="components"} onSelect={() => handleSelect("components")}>Components</TabButton>
-            <TabButton isSelected={selectedTopic==="jsx"} onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic==="props"} onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton isSelected={selectedTopic==="state"} onSelect={() => handleSelect("state")}>State</TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
