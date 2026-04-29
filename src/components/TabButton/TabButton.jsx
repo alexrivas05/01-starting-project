f799601518002865d269146f22096e692828f6ff
alexrivas05 <alex12rivas34@gmail.com>
2026-04-28 23:37:55 -0400

    Forwarded props on tab button

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,12 +1,9 @@
 import './TabButton.css';
 
-export default function TabButton({children, onSelect, isSelected}){
-    function handleClick(){
-        console.log('Hello World');
-    }
-    return(
-        <li>
-            <button className = {isSelected ? 'active' : ''} onClick={onSelect}>{children}</button>
-        </li>
-    )
+export default function TabButton({ children, isSelected, ...props }) {
+  return (
+    <li>
+      <button className={isSelected ? 'active' : ''} {...props}>{children}</button>
+    </li>
+  );
 }
